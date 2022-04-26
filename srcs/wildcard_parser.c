e8f86614fcdf5696011c35c782e2cd945297b379
Rogelio Rast <rrast@at-j4.msk.21-school.ru>
2022-04-26 16:45:59 +0300

    default

--- srcs/wildcard_parser.c
+++ srcs/wildcard_parser.c
@@ -1,6 +1,28 @@
 #include "../includes/minishell.h"
 
-int	connect_array(char ***args, char **wild, int i)
+int	connect_arrays(int i, char ***temp, char ***args, char ***wild)
+{
+	int	j;
+	int	x;
+
+	j = -1;
+	x = -1;
+	while (++j < i)
+		(*temp)[j] = (*args)[j];
+	while ((*wild)[++x])
+		(*temp)[j] = (*wild)[x];
+	x = i;
+	while ((*args)[x])
+	{
+		(*temp)[j] = (*args)[x];
+		j++;
+		x++;
+	}
+	(*temp)[j] = NULL;
+	free((*wild));
+}
+
+int	check_wild(char ***args, char **wild, int i)
 {
 	int		j;
 	int		x;
@@ -23,24 +45,11 @@ int	connect_array(char ***args, char **wild, int i)
 		while (wild[j])
 			free(wild[j++]);
 		free(wild);
+		printf("Error malloc in parse\n");
+		errno = 12;
 		return (-1);
 	}
-	j = -1;
-	x = -1;
-	while (++j < i)
-		temp[j] = (*args)[j];
-	while (wild[++x])
-		temp[j] = wild[x];
-	x = i;
-	while ((*args)[x])
-	{
-		temp[j] = (*args)[x];
-		j++;
-		x++;
-	}
-	temp[j] = NULL;
-	free(wild);
-	return (i);
+	return (connect_arrays);
 }
 
 int	check_wildcard_arg(char ***args)
@@ -51,7 +60,7 @@ int	check_wildcard_arg(char ***args)
 	while ((*args)[i])
 	{
 		if (ft_strchr((*args)[i], '*'))
-			i = connect_array(args, wildcard((*args)[i]), i);
+			i = check_wild(args, wildcard((*args)[i]), i);
 		if (i < 0)
 			return (1);
 		i++;
