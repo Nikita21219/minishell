12f8c50930f1a027ea095ba25dfc6a8779eab3aa
Beldecar Clarinda <bclarind@at-j6.msk.21-school.ru>
2022-04-26 15:05:32 +0300

    fix >>

--- srcs/launcher.c
+++ srcs/launcher.c
@@ -74,9 +74,14 @@ void	set_next_ptr_data_and_free_path(t_data *data, char *path)
 	if (is_same_lines(data->comm->oper, ">") || \
 	is_same_lines(data->comm->oper, ">>"))
 	{
-		while (data->comm && (is_same_lines(data->comm->oper, ">") || \
-		is_same_lines(data->comm->prev->oper, ">")))
-			data->comm = data->comm->next;
+		if (is_same_lines(data->comm->oper, ">>"))
+			data->comm = data->comm->next->next;
+		else
+		{
+			while (data->comm && (is_same_lines(data->comm->oper, ">") || \
+			(data->comm->prev && is_same_lines(data->comm->prev->oper, ">"))))
+				data->comm = data->comm->next;
+		}
 	}
 	else if (is_same_lines(data->comm->oper, "<"))
 	{
