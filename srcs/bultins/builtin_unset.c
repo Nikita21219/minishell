428d02534381b6554a5893f8ae9ec55c6d8d8964
Rogelio Rast <rrast@at-j4.msk.21-school.ru>
2022-05-13 10:07:40 +0300

    rewrite bultins export and unset for local vars

---
+++ srcs/bultins/builtin_unset.c
@@ -0,0 +1,52 @@
+#include "../../includes/minishell.h"
+
+void	free_one_list(t_envr **env)
+{
+	free((*env)->key);
+	free((*env)->val);
+	free (*env);
+}
+
+void	ft_unset_del(t_envr **p2, t_envr **envar, char *arg)
+{
+	t_envr	*p1;
+
+	p1 = *envar;
+	if (is_same_lines(p1->key, arg))
+	{
+		*envar = (*envar)->next;
+		free_one_list(&p1);
+		return ;
+	}
+	while (p1->next != *p2)
+		p1 = p1->next;
+	if ((*p2)->next == NULL)
+	{
+		free_one_list(p2);
+		p1->next = NULL;
+	}
+	else
+	{
+		p1->next = (*p2)->next;
+		free_one_list(p2);
+	}
+	return ;
+}
+
+void	ft_unset(t_data	*data)
+{
+	t_envr	*p2;
+	int		i;
+
+	i = 1;
+	while (data->comm->args[i])
+	{
+		p2 = take_path_env(&data->env, data->comm->args[i]);
+		if (p2)
+			ft_unset_del(&p2, &data->env, data->comm->args[i]);
+		p2 = take_path_env(&data->vars, data->comm->args[i]);
+		if (p2)
+			ft_unset_del(&p2, &data->vars, data->comm->args[i]);
+		i++;
+	}
+}
