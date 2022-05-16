d5194787a196514db8e6065cf72df83e571e3c60
Rogelio Rast <rrast@at-j4.msk.21-school.ru>
2022-05-16 20:09:28 +0300

    fix wild

--- srcs/wildcard/wildcard_parser.c
+++ srcs/wildcard/wildcard_parser.c
@@ -78,6 +78,22 @@ int	check_wildcard_arg(t_comm *tmp)
 	int		a;
 
 	a = -1;
+	if (ft_strchr(tmp->comm, -42))
+	{
+		pos = NULL;
+		if (take_pos_for_wild(&tmp->comm, &pos))
+			return (1);
+		wild = wildcard(tmp->comm, pos);
+		free (pos);
+		if (wild)
+		{
+			tmp->comm = ft_strdup(wild[0]);
+			while (wild[++a])
+				free(wild[a]);
+			free(wild);
+		}
+		a = -1;
+	}
 	while (tmp->args[++a])
 	{
 		pos = NULL;
