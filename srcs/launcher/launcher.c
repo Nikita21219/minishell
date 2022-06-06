7a926d6b3d0fffa3530be5502cbc9599293d8e27
Beldecar Clarinda <bclarind@at-j2.msk.21-school.ru>
2022-06-06 12:05:45 +0300

    fixed .. . /

--- srcs/launcher/launcher.c
+++ srcs/launcher/launcher.c
@@ -6,7 +6,7 @@
 /*   By: bclarind <bclarind@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 16:05:41 by bclarind          #+#    #+#             */
-/*   Updated: 2022/06/06 09:34:28 by bclarind         ###   ########.fr       */
+/*   Updated: 2022/06/06 12:03:53 by bclarind         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,8 @@ int	check_builtins(t_data *data, char **path)
 {
 	int	error;
 
+	if (check_path(data->comm->comm))
+		return (1);
 	if (is_builtins_in_main_proc(data->comm->comm, data))
 	{
 		error = launch_builtins(data);
@@ -92,7 +94,7 @@ int	check_builtins(t_data *data, char **path)
 
 void	set_next_ptr_data_and_free_path(t_data *data, char *path)
 {
-	(void) path;
+	(void) path; //FIXME delete line
 	if (data->comm && (is_same_lines(data->comm->oper, ">") || \
 	is_same_lines(data->comm->oper, ">>") || \
 	is_same_lines(data->comm->oper, "<<") || \
@@ -137,7 +139,11 @@ int	launcher(t_data *data)
 		else if (result < 0)
 			return (1);
 		if (check_builtins(data, &path))
+		{
+			while (data->comm && !is_logic_oper(data->comm->oper))
+				data->comm = data->comm->next;
 			continue ;
+		}
 		wait_count++;
 		result = executor(data, path, count_command);
 		if (result < 0 || result == 1)
